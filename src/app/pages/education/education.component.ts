d28dd55a3935979f866059415fcf22c66e6f1dfb
aaron-plahn <aplahn85@gmail.com>
2020-10-11 09:44:45 -0700

    feat(education): Add content for education page
    Data is stored in a typescript array. Each program is an additional entry in the array.
    NgFor is used to loop over the items in this area.

--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EducationItem } from '../../types/types';
 
 @Component({
   selector: 'app-education',
@@ -6,6 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+  udacity: EducationItem = {
+    school: "Udacity",
+    program: "Deep Learning Nanodegree",
+    startDate: "Aug. 2020",
+    endDate: "Oct. 2020",
+    bullets: [
+      "Tools: Python, PyTorch, NumPy, Pandas, AWS Sagemaker, Lambda, API Gateway",
+      "Deep Neural Networks, Convolutional Neural Networks, Generative Adversarial Networks, Recurrent Neural Networks"
+    ]
+  }
+
+  sfu: EducationItem = {
+    school: "Simon Fraser University",
+    program: "MSc",
+    acronym: "SFU",
+    major: "Physics",
+    startDate: "Sept. 2012",
+    endDate: "Jan. 2015",
+    bullets: [
+      "MSc Thesis: Cosmological constraints on new scalar gravitational interactions",
+      "Tools: Fortran, Python, PCA, Scientific Computing, CAMB",
+      "Supervisor: Dr. Levon Pogosian",
+      "Extracurricular: CASCA Grad Rep, Physics Program Grad Rep, Lead scholarship writing workshops",
+      "Completed all required coursework for PhD (All but dissertation)"
+    ]
+  }
+
+  tru: EducationItem = {
+    school: "Thompson Rivers University",
+    acronym: "TRU",
+    program: "BSc",
+    major: "Physics",
+    minor: "Math",
+    startDate: "Sept. 2008",
+    endDate: "April 2012",
+    bullets: [
+      "4.26 / 4.33 GPA",
+      "Awarded TRU Science Medal as top graduating undergraduate BSc student",
+      "Part of team to reestablish the Physics club and found the Physics Help Centre"
+    ]
+  }
+
+  items: EducationItem[] = [
+    this.udacity,
+    this.sfu,
+    this.tru
+  ];
+
 
   constructor() { }
 
--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -3,3 +3,14 @@ export type NavbarItem = {
     display: string,
     routerLink: string
 }
+
+export type EducationItem = {
+    school: string,
+    program: string,
+    acronym?: string,
+    major?: string,
+    minor?: string,
+    startDate?: string,
+    endDate?: string,
+    bullets: string[]
+}
